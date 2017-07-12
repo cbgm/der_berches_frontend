809c3c2cac26e70cc58ce3947a1aa4e0dae5984a
ChrBerch <christian.bergmann@huk-coburg.de>
2017-07-12 15:51:16 +0200

    Add footer box model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,27 @@
 import { Component, HostListener } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app works!';
-     public hide: boolean = false;
+    title = 'app works!';
+    public hide: boolean = false;
     public show: boolean = true;
-    
+
     @HostListener("window:scroll", [])
-  onWindowScroll() {
-         
-         let Test = window.pageYOffset;
-    if (Test > 50) {
-        console.log(Test);
-        this.hide = true;
-        this.show = false;
-    } else if (this.hide && Test < 50) {
-      this.hide = false;
-        this.show = true;
+    onWindowScroll() {
+
+        let Test = window.pageYOffset;
+        if (Test > 50) {
+            console.log(Test);
+            this.hide = true;
+            this.show = false;
+        } else if (this.hide && Test < 50) {
+            this.hide = false;
+            this.show = true;
+        }
+
     }
-         
-  }
 }
